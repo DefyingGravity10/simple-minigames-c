7e78b25a8a8b9c95243f60206f455309dc94e166
DefyingGravity10 <jeanneclarissetoledo@gmail.com>
2021-08-07 17:54:39 +0800

    Added a function which checks whether or not the player won the game (incomplete).

--- ttt.c
+++ ttt.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 int game_proper(char pos[]); 
+int check_if_match(char pos[], int num);
 int main()
 {
     char pos[10] = {'1', '2', '3', '4', '5', '6', '7', '8', '9'};
@@ -39,6 +40,56 @@ int game_proper(char pos[]) {
         } while(item < 1 || item > 9); //Make sure that the player does not input a position that is no longer available.
 
         (i%2+1 == 1) ? (pos[item-1] = 'X') : (pos[item-1] = 'O');
+
+        if (check_if_match(pos, item)) {
+            printf("Player %d wins!", i%2+1);
+            break;
+        }
+
         item = 0;
+    }
+}
+
+int check_if_match(char pos[], int num) {
+    //Checks if 3 consecutive blocks are filled with the same symbol.
+
+    //To be updated.
+    switch(num) {
+        case(1): {
+            printf("1");
+            break;
+        }
+        case(2): {
+            printf("2");
+            break;
+        }
+        case(3): {
+            printf("3");
+            break;
+        }
+        case(4): {
+            printf("4");
+            break;
+        }
+        case(5): {
+            printf("5");
+            break;
+        }
+        case(6): {
+            printf("6");
+            break;
+        }
+        case(7): {
+            printf("7");
+            break;
+        }
+        case(8): {
+            printf("8");
+            break;
+        }
+        case(9): {
+            printf("9");
+            break;
+        }
     }
 }
